7e3ad724cf2390e1cf7a7031834c880eaf6aecef
Eric Wieser <wieser.eric@gmail.com>
2016-01-05 09:00:00 -0500

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='tamproxy',
+    version='0.0.1',
+
+    description='TAMProxy Python Host',
+    url='https://github.com/mitchgu/TAMProxy-pyHost',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='maslab tamproxy',
+    packages=['tamproxy'],
+    install_requires=['numpy', 'pyserial>=3.0', 'PyYAML'],
+)
