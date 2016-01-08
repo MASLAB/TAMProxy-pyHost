5aba8a016731b20444a357817f98dfeb9efd9886
Gurtej Kanwar <gurtej@mit.edu>
2016-01-08 15:52:45 -0500

    Adds Gyro device and example

---
+++ gyro_read.py
@@ -0,0 +1,23 @@
+from tamproxy import Sketch, SyncedSketch, Timer
+from tamproxy.devices import Gyro
+
+# Prints integrated Gyro readings
+
+class GyroRead(SyncedSketch):
+
+    # Set me!
+    ss_pin = 10
+
+    def setup(self):
+        self.gyro = Gyro(self.tamp, self.ss_pin, integrate=True)
+        self.timer = Timer()
+
+    def loop(self):
+        if self.timer.millis() > 100:
+            self.timer.reset()
+            # Valid gyro status is [0,1], see datasheet on ST1:ST0 bits
+            print self.gyro.val, self.gyro.status
+            
+if __name__ == "__main__":
+    sketch = GyroRead(1, -0.00001, 100)
+    sketch.run()
