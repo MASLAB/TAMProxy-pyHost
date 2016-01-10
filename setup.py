8590ae784dc0deeaab447f91bc2023a397f2b462
Mitchell Gu <me@mitchgu.com>
2016-01-09 19:34:43 -0500

    v0.0.5, update readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tamproxy',
-    version='0.0.4',
+    version='0.0.5',
 
     description='TAMProxy Python Host',
     url='https://github.com/mitchgu/TAMProxy-pyHost',
