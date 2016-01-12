d258f26e4cab704646f63fbd1ead7edaf1b06faf
Eric Wieser <wieser.eric@gmail.com>
2016-01-12 13:23:16 -0500

    Tweak and enable config for logging

--- tamproxy/__init__.py
+++ tamproxy/__init__.py
@@ -1,3 +1,11 @@
 from .utils import dotdict, Timer, config
 from .tamproxy import TAMProxy
 from .sketch import Sketch, SyncedSketch
+
+def _init_logging():
+    # do this in a function to avoid exposing globals
+    from utils import config as c
+    import logging.config
+    logging.config.dictConfig(c.logging)
+
+_init_logging()
