63c017caf9c0c724af784b4190a17aa480fa1dec
Eric Wieser <wieser.eric@gmail.com>
2016-01-19 20:13:28 -0500

    Store raw word as returned from gyro, and display it in the test

--- gyro_read.py
+++ gyro_read.py
@@ -16,8 +16,8 @@ class GyroRead(SyncedSketch):
         if self.timer.millis() > 100:
             self.timer.reset()
             # Valid gyro status is [0,1], see datasheet on ST1:ST0 bits
-            print self.gyro.val, self.gyro.status
-            
+            print "{:6f}, raw: 0x{:08x} = 0b{:032b}".format(self.gyro.val, self.gyro.raw, self.gyro.raw)
+
 if __name__ == "__main__":
     sketch = GyroRead(1, -0.00001, 100)
     sketch.run()
