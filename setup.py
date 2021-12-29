5397795545810cec85f0a03699408059f9239db2
Eric Boehlke <eric.p.boehlke@gmail.com>
2021-12-28 21:45:57 -0600

    Update url and version number

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='tamproxy',
-    version='0.0.7',
+    version='0.1.0',
 
     description='TAMProxy Python Host',
-    url='https://github.com/mitchgu/TAMProxy-pyHost',
+    url='https://github.com/MASLAB/TAMProxy-pyHost',
 
     classifiers=[
         'Development Status :: 3 - Alpha',
