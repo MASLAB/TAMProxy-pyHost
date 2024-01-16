687a8698292022f637029a5befff7b8618121f73
MASLAB Staff <maslab-2024-staff@mit.edu>
2024-01-16 16:44:58 -0500

    Cleaned up and updated examples for feedback motor.

--- feedback_motor_pos_write.py
+++ feedback_motor_pos_write.py
@@ -2,6 +2,8 @@ from tamproxy import Sketch, SyncedSketch, Timer
 from tamproxy.devices import FeedbackMotor, DigitalOutput
 import math
 
+import logging
+
 class FeedbackMotorWrite(Sketch):
     ENC_VCC = 30
     ENC_GND = 29
@@ -17,16 +19,21 @@ class FeedbackMotorWrite(Sketch):
         self.motor.write(0.0)
         self.motorangle = 360.0
         self.timer = Timer()
+        self.debug_timer = Timer()
 
         self.GR = 50.0 # gear ratio
         self.CPR = 64.0 # counts per revolution
+        
+        self.logger = logging.getLogger('tamproxy')
 
     def loop(self):
-        self.motor.write(self.motorangle)
-        if (self.timer.millis() > 100):
+        if (self.timer.millis() > 5):
+            self.motor.write(self.motorangle)
             self.timer.reset()
-            print("Desired Angle: {} Measured Angle: {}".format(self.motorangle, 360.0*self.motor.enc_count/(self.GR*self.CPR)))
-
+        if (self.debug_timer.millis() > 1000):
+            self.logger.info("Desired Angle: {} Measured Angle: {}".format(self.motorangle, 360.0*self.motor.enc_count/(self.GR*self.CPR)))
+            self.timer.reset()
+    
 if __name__ == "__main__":
     sketch = FeedbackMotorWrite()
     sketch.run()
--- feedback_motor_vel_write.py
+++ feedback_motor_vel_write.py
@@ -1,46 +1,37 @@
 from tamproxy import Sketch, SyncedSketch, Timer
 from tamproxy.devices import FeedbackMotor, DigitalOutput
 import math
+import logging
 
 class FeedbackMotorWrite(Sketch):
-    ENC_VCC_left = 10
-    ENC_GND_left = 11
-
-    ENC_VCC_right = 38
-    ENC_GND_right = 39
+    ENC_VCC = 30
+    ENC_GND = 29
 
     def setup(self):
-        self.enc_power_left = DigitalOutput(self.tamp, self.ENC_VCC_left)
-        self.enc_ground_left = DigitalOutput(self.tamp, self.ENC_GND_left)
-        self.enc_power_left.write(True)
-        self.enc_ground_left.write(False)
+        self.enc_power = DigitalOutput(self.tamp, self.ENC_VCC)
+        self.enc_ground = DigitalOutput(self.tamp, self.ENC_GND)
+        self.enc_power.write(True)
+        self.enc_ground.write(False)
 
-        # self.enc_power_right = DigitalOutput(self.tamp, self.ENC_VCC_right)
-        # self.enc_ground_right = DigitalOutput(self.tamp, self.ENC_GND_right)
-        # self.enc_power_right.write(True)
-        # self.enc_ground_right.write(False)
-
-        self.left_motor = FeedbackMotor(self.tamp, 16, 15, 32, 31, True)
-        self.left_motor_vel = 0.1
+        self.motor = FeedbackMotor(self.tamp, 16, 15, 32, 31, True)
+        self.motor_vel_rad_per_s = 6.28
         
-        # self.right_motor = FeedbackMotor(self.tamp, 4, 2, 6, 37, 36, True)
-        # self.right_motor_vel = -22.0
-
-        self.left_motor.write(0.0)
-        # self.right_motor.write(0.0)
         self.timer = Timer()
+        self.debug_timer = Timer()
 
         self.GR = 50.0 # gear ratio
         self.CPR = 64.0 # counts per revolution
 
-    def loop(self):
-        self.left_motor.write(self.left_motor_vel)
-        # self.right_motor.write(self.right_motor_vel)
-        if (self.timer.millis() > 100):
-            self.timer.reset()
-            #print("Left Vd: {} V: {:.4f}   Right Vd: {} V: {:.4f}".format(self.left_motor_vel, self.left_motor.estimated_velocity, self.right_motor_vel, self.right_motor.estimated_velocity))
-            print("Left Vd: {} V: {:.4f} ".format(self.left_motor_vel, self.left_motor.estimated_velocity))
+        self.logger = logging.getLogger('tamproxy')
 
+    def loop(self):
+        if (self.timer.millis() > 5):
+            self.motor.write(self.motor_vel_rad_per_s)
+            self.timer.reset()
+        if (self.debug_timer.millis() > 100):
+            self.logger.info("Desired V: {} Measured V: {}".format(self.motor, self.motor.estimated_velocity))
+            self.debug_timer.reset()
+            
 if __name__ == "__main__":
     sketch = FeedbackMotorWrite()
     sketch.run()
