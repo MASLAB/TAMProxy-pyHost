4a40570aba6827b486eb69c0266d458abb618dcb
MASLAB Staff <maslab-2024-staff@mit.edu>
2024-01-25 18:26:23 -0500

    Modify imu

--- imu_read.py
+++ imu_read.py
@@ -6,18 +6,21 @@ from tamproxy.devices import Imu
 
 class ImuRead(SyncedSketch):
 
+    # Set me!
+    di_pin = 20
+
     def setup(self):
-        self.imu = Imu(self.tamp)
+        self.imu = Imu(self.tamp, self.di_pin)
         self.timer = Timer()
 
     def loop(self):
-        if self.timer.millis() > 100:
+        if self.timer.millis() > 1000:
             self.timer.reset()
-            print("Accel x: {} g, y: {} g, z: {} g".format(
+            print("Accel x: {} m/s^2, y: {} m/s^2, z: {} m/s^2".format(
                 self.imu.ax, self.imu.ay, self.imu.az))
-            print("Gyro x: {} deg/sec, y: {} deg/sec, z: {} deg/sec".format(
+            print("Gyro x: {} rad/sec, y: {} rad/sec, z: {} rad/sec".format(
                 self.imu.gx, self.imu.gy, self.imu.gz))
-            print("Mag x: {} mG, y: {} mG, z: {} mG".format(
+            print("Mag x: {} uT, y: {} uT, z: {} uT\n".format(
                 self.imu.mx, self.imu.my, self.imu.mz))
 
 if __name__ == "__main__":
--- tamproxy/devices/imu.py
+++ tamproxy/devices/imu.py
@@ -1,4 +1,4 @@
-from .device import Device
+from .device import ContinuousReadDevice
 from .. import config as c
 
 # Unsigned 2 signed
@@ -6,20 +6,13 @@ def us2s(unsigned):
     signed = unsigned - 65536 if unsigned > 32767 else unsigned
     return signed
 
-# Retreives continuous time-of-flight distance measurement in millimeters.
-class Imu(Device):
+class Imu(ContinuousReadDevice):
 
     DEVICE_CODE = c.devices.imu.code
     READ_CODE   = c.devices.imu.read_code
 
-    DEFAULT_ARES = 2.0 / 32768.0
-    DEFAULT_GRES = 250.0 / 32768.0
-    DEFAULT_MRES = 10 * 4912 / 32760
-
-    def __init__(self, tamproxy, continuous=True):
-        super(Imu, self).__init__(tamproxy)
-        if continuous:
-            self.start_continuous()
+    def __init__(self, tamproxy, dipin, continuous=True):
+        self.dipin = dipin
         self.ax = 0
         self.ay = 0
         self.az = 0
@@ -29,43 +22,25 @@ class Imu(Device):
         self.mx = 0
         self.my = 0
         self.mz = 0
+        super(Imu, self).__init__(tamproxy, continuous)
+
+    def __repr__(self):
+        return super(Imu, self).__repr__(self.dipin)
 
     @property
     def add_payload(self):
-        return self.DEVICE_CODE
+        return self.DEVICE_CODE + chr(self.dipin)
 
-    def update(self):
-        self.tamp.send_request(self.id, self.READ_CODE, self.handle_update)
-
-    def start_continuous(self, weight=1):
-        print("Starting continuous")
-        self.tamp.send_request(self.id, self.READ_CODE, self.handle_update,
-                               continuous=True, weight=weight)
-
-    def stop_continuous(self):
-        self.tamp.send_request(self.id, self.READ_CODE, self.tamp.empty_callback,
-                               continuous=True, weight=1, remove=True)
-
-    def handle_update(self, request, response):
+    def _handle_update(self, request, response):
+        # print(response)
         assert len(response) == 18
-        ax = us2s((response[0]<<8) + response[1])
-        ay = us2s((response[2]<<8) + response[3])
-        az = us2s((response[4]<<8) + response[5])
-        gx = us2s((response[6]<<8) + response[7])
-        gy = us2s((response[8]<<8) + response[9])
-        gz = us2s((response[10]<<8) + response[11])
-        mx = us2s((response[12]<<8) + response[13])
-        my = us2s((response[14]<<8) + response[15])
-        mz = us2s((response[16]<<8) + response[17])
-
-        # scale values based on default sensor resolution
-        self.ax = ax * self.DEFAULT_ARES
-        self.ay = ay * self.DEFAULT_ARES
-        self.az = az * self.DEFAULT_ARES
-        self.gx = gx * self.DEFAULT_GRES
-        self.gy = gy * self.DEFAULT_GRES
-        self.gz = gz * self.DEFAULT_GRES
-        self.mx = mx * self.DEFAULT_MRES
-        self.my = my * self.DEFAULT_MRES
-        self.mz = mz * self.DEFAULT_MRES
+        self.ax = us2s((response[0]<<8) | response[1]) / 1000.0
+        self.ay = us2s((response[2]<<8) | response[3]) / 1000.0
+        self.az = us2s((response[4]<<8) | response[5]) / 1000.0
+        self.gx = us2s((response[6]<<8) | response[7]) / 1000.0
+        self.gy = us2s((response[8]<<8) | response[9]) / 1000.0
+        self.gz = us2s((response[10]<<8) | response[11]) / 1000.0
+        self.mx = us2s((response[12]<<8) | response[13]) / 1000.0
+        self.my = us2s((response[14]<<8) | response[15]) / 1000.0
+        self.mz = us2s((response[16]<<8) | response[17]) / 1000.0
 
